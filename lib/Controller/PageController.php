67a8ab941bd4cdd13ae48798016a51fb438cc95b
Daniel Kesselberg <mail@danielkesselberg.de>
2024-10-14 18:58:22 +0200

    chore: Add spdx headers
    Signed-off-by: Daniel Kesselberg <mail@danielkesselberg.de>

--- lib/Controller/PageController.php
+++ lib/Controller/PageController.php
@@ -2,8 +2,10 @@
 
 declare(strict_types=1);
 
-// SPDX-FileCopyrightText: Hamza Mahjoubi <hamzamahjoubi22@proton.me>
-// SPDX-License-Identifier: AGPL-3.0-or-later
+/**
+ * SPDX-FileCopyrightText: 2024 Hamza Mahjoubi <hamzamahjoubi22@proton.me>
+ * SPDX-License-Identifier: AGPL-3.0-or-later
+ */
 
 namespace OCA\Files_EmailViewer\Controller;
 
